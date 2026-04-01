d59b99e3f87a61a1be78691dc34e00f48cdcea69
Obong <obeekings>
2026-04-01 12:22:24 +0100

    done, over to the next

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react' 
+
+ const messages = [
+        'Learn React ⚛️',
+        'Apply for jobs 💼',
+        'Invest your new income 🤑',
+      ];
+
+function App() {
+
+ const [step, setStep] = useState(1);
+
+ function handlePrevious(){
+  if (step > 1) {
+    setStep(step - 1)
+  };
+ }
+ function handleNext(){
+if (step < 3) {
+  setStep(step + 1)
+}
+ }
+  // const step =  1;
+  return (
+    <div className='steps'>
+
+      <div className='numbers'>
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
+      </div>
+        <p className='message'>step {step}: {messages[step - 1]}</p>
+
+        <div className='buttons'>
+          <button onClick={handlePrevious} style={{backgroundColor: "#7950f2", color: "#fff"}}>Previous </button>
+
+          <button onClick={handleNext} style={{backgroundColor: "#7950f2", color: "#fff"}}>Next</button>
+        </div>
+    </div>
+  )
+}
+
+export default App
